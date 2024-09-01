a577c6d4dfe6ec68ecd7338d9f9d0862319622a3
arika <arika.khor@gmail.com>
2024-09-01 10:58:48 -0500

    make miniGW

---
+++ cmake-build-debug/CMakeFiles/CMAKE_C_STANDARD.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMAKE_C_STANDARD.exe"
+  "CMAKE_C_STANDARD.exe.manifest"
+  "CMAKE_C_STANDARD.pdb"
+  "CMakeFiles/CMAKE_C_STANDARD.dir/Hw1.c.obj"
+  "CMakeFiles/CMAKE_C_STANDARD.dir/Hw1.c.obj.d"
+  "CMakeFiles/CMAKE_C_STANDARD.dir/main.c.obj"
+  "CMakeFiles/CMAKE_C_STANDARD.dir/main.c.obj.d"
+  "libCMAKE_C_STANDARD.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CMAKE_C_STANDARD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CMAKE_C_STANDARD.dir/main.c.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/CMAKE_C_STANDARD.dir/main.c.obj: \
+ C:\Users\arika\CLionProjects\CS3203\main.c
