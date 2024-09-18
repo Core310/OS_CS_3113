d2704da2dd8cbbeade62ed0241378e65b2184398
Something Something <arika.khor@gmail.com>
2024-09-18 15:13:38 -0500

    init cygwin

--- assignment1.c
+++ assignment1.c
@@ -1,35 +1,33 @@
-//
-// Created by arika on 17/9/2024.
-//
-#include <unistd.h>
+// C program to demonstrate working of wait()
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main(void) {
-    int p1=1,p2=1,p3=1,p4=1;
-    
 
+void incrementByOne(int oldVal ,int newVal) {
+    while (oldVal <= newVal) oldVal++;
+    return; oldVal;
+}
+
+int main() {
+    int total=0;
+    pid_t pid;
+
+   pid == 0 ? incrementByOne(total,10000) : ();
+
+    return 0;
 }
 
 
 
-/*
-*In this assignment, you will create 4 processes. Each of these processes will share a variable called
+/**
+ * Pid ==0 when the process is finished, hence when the child process is finished.
+ *
+In this assignment, you will create 4 processes. Each of these processes will share a variable called
 "total". Each will increment the variable “total” by one to 100,000, 200,000, 300,000 and 500,000
 respectively. Make sure that only the newly created child calls the function “process#()”
-After all the children have finished, the parent process should release the shared memory and terminate.
-Use the "wait" function so that the parent knows precisely when each of the children finishes. The
-parent should print the process id of each child as the child finishes execution. Then it should release
-shared memory and print "End of Program".
-You need to run the program several times and analyze your observations (write a detailed report).
-Sample output
-From Process 1: counter = 270547.
-From Process 2: counter = 347860.
-From Process 3: counter = 400001.
-From Process 4: counter = 500000.
-Child with ID: 2412 has just exited.
-Child with ID: 2411 has just exited.
-Child with ID: 2413 has just exited.
-Child with ID: 2415 has just exited.
-End of Simulation.
 
- */
+
+ *
+ **/
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/gitRepos/OS_CS_3113
+# Install script for directory: C:/Users/arika/CLionProjects/OS_CS_3113
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/gitRepos/OS_CS_3113/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/arika/CLionProjects/OS_CS_3113/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
