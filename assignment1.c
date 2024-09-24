92639c295b823f634f470724b8acc5dd6f001fb4
dockerContainer <arikak@ou.edu>
2024-09-24 22:04:08 +0000

    init final code version

--- assignment1.c
+++ assignment1.c
@@ -6,36 +6,26 @@
 #include <sys/shm.h>
 
 #define SHMKEY ((key_t) 1497)
-
 typedef struct {
     int value;
 } shared_mem;
+shared_mem *total;
 
 int incrementByOne(int oldVal, int newVal) {
     while (oldVal < newVal) oldVal++;
     return oldVal;
 }
 
-int createProcessAfter(int total, int processNumber, int valTo) {
+int createProcessAfter(int total, int valTo, char processNum) {
     total = incrementByOne(total, valTo);
-    printf("From Process %d: counter = %d\n", processNumber, total);
+    printf("From Process %c: counter = %d\n", processNum, total);
     return total;
 }
 
-void waitForPID() {
-    wait(getpid());
-    printf("Child with ID: %d has just exited.\n", getpid());
-}
-
-void makeChildProcess(shared_mem *total, int *p, int valTo) {
-    total->value = createProcessAfter(total->value, 1, valTo);
-    *p = fork();
-    waitForPID();
-}
-
 int main() {
-    int shmid;
-    shared_mem *total;
+    int shmid, pid1, pid2, pid3, pid4;
+    char *shmadd;
+    shmadd = (char *) 0;
 
     if ((shmid = shmget(SHMKEY, sizeof(shared_mem), IPC_CREAT | 0666)) < 0) {
         perror("shmget");
@@ -47,18 +37,24 @@ int main() {
     }
 
     total->value = 0;
-    int p1, p2 = 1, p3 = 3, p4 = 4;
-    p1 = fork();
-    p1 == 0 ? makeChildProcess(total, &p2, 100000) : 0;
-    p2 == 0 ? makeChildProcess(total, &p3, 200000) : 0;
-    p3 == 0 ? makeChildProcess(total, &p4, 300000) : 0;
-    p4 == 0 ? (createProcessAfter(total->value, 4, 500000), waitForPID()) : 0;
+
+    (pid1 = fork()) == 0 ? (createProcessAfter(total->value, 10000, '1'), exit(0)) : 0;
+    (pid2 = fork()) == 0 ? (createProcessAfter(total->value, 20000, '2'), exit(0)) : 0;
+    (pid3 = fork()) == 0 ? (createProcessAfter(total->value, 30000, '3'), exit(0)) : 0;
+    (pid4 = fork()) == 0 ? (createProcessAfter(total->value, 40000, '4'), exit(0)) : 0;
+    for (int i = 0; i < 4; i++) {
+        int status;
+        pid_t child_pid = wait(&status);
+        printf("Child with ID: %d has just exited.\n", child_pid);
+    }
 
     if (shmdt(total) == -1) {
-        perror ("shmdt");
-        exit (-1);
+        perror("shmdt");
+        exit(-1);
     }
-    shmctl(shmid, IPC_RMID,NULL);
+    shmctl(shmid, IPC_RMID, NULL);
+
+    printf("End of Simulation.");
 
     return 0;
 }
