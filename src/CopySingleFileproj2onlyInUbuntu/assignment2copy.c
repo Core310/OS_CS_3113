60acf3288ed2df6db421d92edb0e2ce3032aa6d1
Something Something <arika.khor@gmail.com>
2024-10-21 14:40:46 -0500

    working semaphore (commit this)

--- src/CopySingleFileproj2onlyInUbuntu/assignment2copy.c
+++ src/CopySingleFileproj2onlyInUbuntu/assignment2copy.c
@@ -4,8 +4,10 @@
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
 
 #define SHMKEY ((key_t) 1497) // macro to define shared memory
+#define SEMKEY ((key_t) 1498) // macro to define semaphore key
 
 /**
  * Struct for shared memory, bound to variable total
@@ -14,6 +16,37 @@ typedef struct {
     int value;
 } shared_mem;
 shared_mem *total;
+
+/* Semaphore union used to initialize semaphore */
+typedef union {
+    int val;
+    struct semid_ds *buf;
+    ushort *array;
+} semunion;
+
+int sem_id;  // semaphore id
+
+/* semaphore buffers for P (wait) and V (signal) operations */
+static struct sembuf OP = {0, -1, 0};  // P operation (wait)
+static struct sembuf OV = {0, 1, 0};   // V operation (signal)
+struct sembuf *P = &OP;
+struct sembuf *V = &OV;
+
+/* Semaphore wait operation */
+void semaphore_wait() {
+    if (semop(sem_id, P, 1) == -1) {
+        perror("semop P");
+        exit(1);
+    }
+}
+
+/* Semaphore signal operation */
+void semaphore_end_signal() {
+    if (semop(sem_id, V, 1) == -1) {
+        perror("semop V");
+        exit(1);
+    }
+}
 
 /**
  * Increments input value (total) to a new declared value, child method to createProcessAfter
@@ -24,59 +57,22 @@ shared_mem *total;
 int incrementByOne(int oldVal, int newVal) {
     while (oldVal < newVal) oldVal++;
     return oldVal;
-}//increment by 100k 200k 300k per each run
-
-
-#include <sys/sem.h>
-
-/* semaphore key */
-#define SEMKEY ((key_t) 400L)
-/* number of semaphores being created */
-#define NSEMS 1
-
-/* GLOBAL */
-
-int sem_id; /* semaphore id */
-/* semaphore buffers */
-static struct sembuf OP = {0,-1,0};
-static struct sembuf OV = {0,1,0};
-struct sembuf *P = &OP;
-struct sembuf *V = &OV;
-
-/* semaphore union used to generate semaphore */
-typedef union {
-    int val;
-    struct semid_ds *buf;
-    ushort *array;
-} semunion;
-
-/* POP (wait()) function for semaphore to protect critical section */
-int POP() {
-    int status;
-    status = semop(sem_id, P, 1);
-    return status;
 }
-
-/* VOP (signal()) function for semaphore to release protection */
-int VOP() {
-    int status;
-    status = semop(sem_id, V, 1);
-    return status;
-}
-
 
 /**
  *
  * @param total input for incrementByOne, representing `total` global variable
  * @param valTo input for incrementByOne
- * @param processNum Current process number assigned by usr
+ * @param processNum Current process number assigned by user
  * @return updated total value and a print of process counter
  */
 int createProcessAfter(int total, int valTo, char processNum) {
-    POP();
+    semaphore_wait();
+
     total = incrementByOne(total, valTo);
     printf("From Process %c: counter = %d\n", processNum, total);
-    VOP();
+
+    semaphore_end_signal();
     return total;
 }
 
@@ -85,60 +81,60 @@ int main() {
     char *shmadd;
     shmadd = (char *) 0;
 
-    int value, value1;
-    semunion semctl_arg;
-    semctl_arg.val = 1;
-
-    // Create semaphore set
-    sem_id = semget(SEMKEY, NSEMS, IPC_CREAT | 0666);
-    if (sem_id < 0) {
-        perror("Error in creating semaphore");
-        return 1;
-    }
-
-    // Initialize semaphore (SETVAL)
-    semctl_arg.val = 1; // set to 1 to ride to rohan
-    if (semctl(sem_id, 0, SETVAL, semctl_arg) == -1) {
-        perror("Error in initializing semaphore");
-        return 1;
-    }
-
+    // make SHM
     if ((shmid = shmget(SHMKEY, sizeof(shared_mem), IPC_CREAT | 0666)) < 0) {
         perror("shmget");
         exit(1);
     }
-
     if ((total = (shared_mem *) shmat(shmid, NULL, 0)) == (shared_mem *) -1) {
         perror("shmat");
         exit(1);
-    } // assist in creating shared mem
+    }
 
-    total->value = 0;
+    total->value = 0;  // Initialize shared memory variable
 
+    // Make Semaphore
+    sem_id = semget(SEMKEY, 1, IPC_CREAT | 0666);
+    if (sem_id == -1) {
+        perror("semget");
+        exit(1);
+    }
+
+    // Init semaphore
+    semunion sem_union;
+    sem_union.val = 1;
+    if (semctl(sem_id, 0, SETVAL, sem_union) == -1) {
+        perror("semctl");
+        exit(1);
+    }
+
+    // Fork and create child processes
     (pid1 = fork()) == 0 ? (createProcessAfter(total->value, 100000, '1'), exit(0)) : 0;
     (pid2 = fork()) == 0 ? (createProcessAfter(total->value, 200000, '2'), exit(0)) : 0;
     (pid3 = fork()) == 0 ? (createProcessAfter(total->value, 300000, '3'), exit(0)) : 0;
     (pid4 = fork()) == 0 ? (createProcessAfter(total->value, 500000, '4'), exit(0)) : 0;
-    //create all child processes
 
-
+    // Wait for all child processes to complete
     for (int i = 0; i < 4; i++) {
         int status;
         pid_t child_pid = wait(&status);
         printf("Child with ID: %d has just exited.\n", child_pid);
-    }//wait for children to exit after kill
+    }
+
+    // Detach and remove shared memory
     if (shmdt(total) == -1) {
         perror("shmdt");
-        exit(-1);
+        exit(1);
     }
-    /* De-allocate semaphore */
-    semctl_arg.val = 0;
-    int status = semctl(sem_id, 0, IPC_RMID, semctl_arg);
-    if(status < 0) printf("Error in removing the semaphore.\n");
+    shmctl(shmid, IPC_RMID, NULL);  // Remove shared memory
 
-    shmctl(shmid, IPC_RMID, NULL);//detach and delete shared memory
+    // -rm semaphore
+    if (semctl(sem_id, 0, IPC_RMID, sem_union) == -1) {
+        perror("semctl IPC_RMID");
+        exit(1);
+    }
 
-    printf("End of Simulation.");
+    printf("End of Simulation.\n");
 
     return 0;
 }
