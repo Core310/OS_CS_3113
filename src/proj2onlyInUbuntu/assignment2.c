9a100b60add776b486f56f7a682a0cccde14a2a0
Something Something <arika.khor@gmail.com>
2024-10-24 11:29:57 -0500

    trying for smaller code bloc (still)

--- src/proj2onlyInUbuntu/assignment2.c
+++ src/proj2onlyInUbuntu/assignment2.c
@@ -1,4 +1,3 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
@@ -8,8 +7,6 @@
 #include <sys/sem.h>
 #include "sharedMemory.h"
 #include "semaphore.h"
-
-
 
 /**
  * Increments input value (total) to a new declared value, child method to createProcessAfter
@@ -39,8 +36,23 @@ void createProcessAfter(int valTo, char processNum) {
     semaphore_end_signal();
 }
 
-void make_and_init_semaphore(semunion *sem_union, int *value, bool *returns) {
-    returns = false;
+int main() {
+    int shmid, pid1, pid2, pid3, pid4;
+    char *shmadd;
+    shmadd = (char *) 0;
+
+    // make SHM
+    if ((shmid = shmget(SHMKEY, sizeof(shared_mem), IPC_CREAT | 0666)) < 0) {
+        perror("shmget");
+        exit(1);
+    }
+    if ((total = (shared_mem *) shmat(shmid, NULL, 0)) == (shared_mem *) -1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    total->value = 0;  // Initialize shared memory variable
+
     // Make Semaphore
     sem_id = semget(SEMKEY, 1, IPC_CREAT | 0666);
     if (sem_id == -1) {
@@ -48,27 +60,25 @@ void make_and_init_semaphore(semunion *sem_union, int *value, bool *returns) {
         exit(1);
     }
 
-    sem_union->val = 1;
-    if (semctl(sem_id, 0, SETVAL, *sem_union) == -1) {
+    // Init semaphore
+    semunion sem_union;
+    sem_union.val = 1;
+    if (semctl(sem_id, 0, SETVAL, sem_union) == -1) {
         perror("semctl");
         exit(1);
     }
-}
 
-void init_shared_memory(int *shmid, int *value, bool *returns) {
-    returns = false;
-    // make SHM
-    if ((*shmid = shmget(SHMKEY, sizeof(shared_mem), IPC_CREAT | 0666)) < 0) {
-        perror("shmget");
-        exit(1);
-    }
-    if ((total = (shared_mem *) shmat(*shmid, NULL, 0)) == (shared_mem *) -1) {
-        perror("shmat");
-        exit(1);
-    }
-}
+    // Fork and create child processes
+    (pid1 = fork()) == 0 ? (createProcessAfter( 100000, '1'), exit(0)) : processIdExiter();
+    (pid2 = fork()) == 0 ? (createProcessAfter( 200000, '2'), exit(0)) : processIdExiter();
+    (pid3 = fork()) == 0 ? (createProcessAfter( 300000, '3'), exit(0)) : processIdExiter();
+    (pid4 = fork()) == 0 ? (createProcessAfter( 500000, '4'), exit(0)) : processIdExiter();
 
-bool remove_sharedMem_and_semaphore(int shmid, semunion sem_union, int *value) {
+    // Wait for all child processes to complete
+    for (int i = 0; i < 4; i++) {
+
+    }
+
     // Detach and remove shared memory
     if (shmdt(total) == -1) {
         perror("shmdt");
@@ -81,34 +91,6 @@ bool remove_sharedMem_and_semaphore(int shmid, semunion sem_union, int *value) {
         perror("semctl IPC_RMID");
         exit(1);
     }
-    return false;
-}
-
-int main() {
-    int shmid, pid1, pid2, pid3, pid4;
-    char *shmadd;
-    shmadd = (char *) 0;
-
-    //init shared memory
-    int value;
-    bool returns;
-    init_shared_memory(&shmid, &value, &returns);
-    if (returns) return value;
-
-    total->value = 0;  // Initialize shared memory variable
-
-    // make & init semaphore
-    semunion sem_union;
-    make_and_init_semaphore(&sem_union, &value, &returns);
-    if (returns) return value;
-
-    // Fork and create child processes
-    (pid1 = fork()) == 0 ? (createProcessAfter( 100000, '1'), exit(0)) : processIdExiter();
-    (pid2 = fork()) == 0 ? (createProcessAfter( 200000, '2'), exit(0)) : processIdExiter();
-    (pid3 = fork()) == 0 ? (createProcessAfter( 300000, '3'), exit(0)) : processIdExiter();
-    (pid4 = fork()) == 0 ? (createProcessAfter( 500000, '4'), exit(0)) : processIdExiter();
-
-    if (remove_sharedMem_and_semaphore(shmid, sem_union, &value)) return value;
 
     printf("End of Simulation.\n");
 
